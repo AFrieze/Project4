e24bbc012d355bb86e137f0ad8516683f66d290f
bryce-leitner-roberts <bryce.leitner.roberts@gmail.com>
2015-11-29 19:14:13 -0500

    Added method back for student status

--- src/main/java/org/gatechproject/project4/BAL/dto/Student.java
+++ src/main/java/org/gatechproject/project4/BAL/dto/Student.java
@@ -3,8 +3,10 @@ package org.gatechproject.project4.BAL.dto;
 import org.gatechprojects.project4.SharedDataModules.User;
 
 public class Student extends Person {
-
 	public Student(User user) {
 		super(user);
+	}
+	public String getStudentStanding(){
+		return "Good";
 	}
 }
