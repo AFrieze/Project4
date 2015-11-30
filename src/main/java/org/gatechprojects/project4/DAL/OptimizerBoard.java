367a8f7df432ad4264aa2a009cc0ff94ff86f76d
nipunanand <nipun109@yahoo.com>
2015-11-30 09:07:56 +0530

    got latest from repo

--- src/main/java/org/gatechprojects/project4/DAL/OptimizerBoard.java
+++ src/main/java/org/gatechprojects/project4/DAL/OptimizerBoard.java
@@ -3,13 +3,17 @@ package org.gatechprojects.project4.DAL;
 import java.util.Calendar;
 import java.util.List;
 
+import org.gatechprojects.project4.SharedDataModules.Course;
 import org.gatechprojects.project4.SharedDataModules.InputStudentCoursePreference;
 import org.gatechprojects.project4.SharedDataModules.OptimizerCalculation;
 import org.gatechprojects.project4.SharedDataModules.OutputOfferedCourse;
 import org.gatechprojects.project4.SharedDataModules.OutputUserCourseAssignment;
+import org.gatechprojects.project4.SharedDataModules.StudentPreference;
+import org.gatechprojects.project4.SharedDataModules.User;
 import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.hibernate.criterion.Order;
 import org.hibernate.criterion.Restrictions;
 
 public class OptimizerBoard extends Board {
@@ -26,7 +30,7 @@ public class OptimizerBoard extends Board {
 
 	public int getCourseDemand(int courseId, int optimizerCalculationId) {
 		Query query = getSession().createQuery(
-				"select count(*) from InputStudentCoursePreference where course.id = :courseId and inputStudent.optimizerCalculation.id = :optimizerCalculationId");
+				"select count(*) from InputStudentCoursePreference where course.id = :courseId and optimizerCalculation.id = :optimizerCalculationId");
 		query.setInteger("optimizerCalculationId", optimizerCalculationId);
 		query.setInteger("courseId", courseId);
 		Integer count = (Integer) query.uniqueResult();
@@ -42,18 +46,32 @@ public class OptimizerBoard extends Board {
 		String hql = "from OptimizerCalculation as c where c.completionTime <= :completionTime and c.isShadow = :isShadow order by c.completionTime desc";
 		List calculations = getSession().createQuery(hql).setParameter("completionTime", calendar)
 				.setParameter("isShadow", isShadow).list();
-		if (calculations.size() > 1) {
+		if (calculations.size() > 0) {
 			OptimizerCalculation optCal = (OptimizerCalculation) calculations.get(0);
 			return optCal.getId();
 		}
 		return -1;
 	}
+	
+	public OptimizerCalculation getOptimizerCalculation(int optimizerCalculationId) {
+		return getSession().get(OptimizerCalculation.class, optimizerCalculationId);
+	}
+	
+	public List<OptimizerCalculation> getLastOptimizerCalculations(int limit) {
+		List<OptimizerCalculation> optimizerSet =  getSession().createCriteria(OptimizerCalculation.class)
+				.addOrder(Order.desc("id")).setFirstResult(0)
+				.setMaxResults(limit).list();
+
+			return optimizerSet;
+
+	}
+	
 
 	public List<InputStudentCoursePreference> getStudentPreferencesForCalculation(int studentID,
 			int optimizerCalculationID) {
 		return getSession().createCriteria(InputStudentCoursePreference.class)
-				.add(Restrictions.eq("inputStudent.user.id", studentID))
-				.add(Restrictions.eq("inputStudent.optimizerCalculation.id", optimizerCalculationID)).list();
+				.add(Restrictions.eq("inputStudent.id", studentID))
+				.add(Restrictions.eq("optimizerCalculation.id", optimizerCalculationID)).list();
 	}
 
 	public List<OutputUserCourseAssignment> getUserCourseAssignments(int optimizerCalculationId, boolean isShadow) {
@@ -61,5 +79,20 @@ public class OptimizerBoard extends Board {
 				.add(Restrictions.eq("optimizerCalculation.id", optimizerCalculationId))
 				.add(Restrictions.eq("optimizerCalculation.isShadow", isShadow)).list();
 	}
+	
+	/**
+	 * Used for testing the method getOptimizerCourseRecommendations() 
+	 */
+	public void createOutputOfferedCourse(Course course, OptimizerCalculation optimizerCalculation) {
+		OutputOfferedCourse offeredCourse = new OutputOfferedCourse();
 
+		offeredCourse.setCourse(course);
+		offeredCourse.setCourseSize(50);
+		offeredCourse.setOptimizerCalculation(optimizerCalculation);
+
+		getSession().save(offeredCourse);
+	}
+
+	
+	
 }
